e8836d2d09234bf70cd2e8da09ec94dd760a9588
Arthur Moraes <135155777+Xenoxys1@users.noreply.github.com>
2025-03-26 16:44:47 -0300

    DevLinks

---
+++ Aulas/AulaReact10/src/App.jsx
@@ -0,0 +1,10 @@
+import "./index.css";
+import { Links } from "./components/links/index.jsx";
+
+export function App() {
+  return (
+    <>
+        <Links />
+    </>
+  );
+}
---
+++ Aulas/AulaReact10/src/components/links/index.jsx
@@ -0,0 +1,63 @@
+import "./styles.css";
+import { Youtube, Github, Instagram, Linkedin } from "lucide-react";
+import { useTheme } from "../../context/ThemeContext";
+import { api } from '../../services/api';
+import { useEffect } from "react";
+import { useState } from "react";
+
+export function Links() {
+  const { toggleTheme } = useTheme();
+
+  const [user, setUser] = useState([]);
+
+  useEffect(() => {
+    async function getUserData() {
+      try {
+        const response = await api.get('/users/Xenoxys1');
+  
+        setUser(response.data);
+        console.log(response);
+      }catch(error){
+        console.log("Erro ao buscar dados do usuário ", error);
+      }
+    }
+
+    getUserData();
+  
+  }, []); 
+
+  console.log(user);
+
+  return (
+    <section>
+      <div id="avatar">
+        <img src={user.avatar_url} alt="avatar" width="112px" height="112px" />
+        <h1>@{user.login}</h1>
+      </div>
+      <label class="switch">
+        <input type="checkbox" onClick={() => toggleTheme()} />
+        <span class="slider round"></span>
+      </label>
+      <div id="linkButtons">
+        <button type="button">Inscreva-se no NLW</button>
+        <button type="button">Baixe meu e-book</button>
+        <button type="button">Veja meu portfólio</button>
+        <button type="button">Conheça o Explorer</button>
+      </div>
+      <div id="socials">
+        <a href="youtube.com" target="_blank">
+          <Youtube size={24} />
+        </a>
+        <a href="https://github.com/Xenoxys1" target="_blank">
+          <Github size={24} />
+        </a>
+        <a href="instagram.com" target="_blank">
+          <Instagram size={24} />
+        </a>
+        <a href="linkedin.com" target="_blank">
+          <Linkedin size={24} />
+        </a>
+      </div>
+    </section>
+  );
+}

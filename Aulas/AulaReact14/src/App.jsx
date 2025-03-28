7111a6a40987dc028d413d3fb069422d79413a42
Arthur Moraes <135155777+Xenoxys1@users.noreply.github.com>
2025-03-28 17:54:50 -0300

    Aula Nº14 Upd

--- Aulas/AulaReact14/src/App.jsx
+++ Aulas/AulaReact14/src/App.jsx
@@ -1,5 +1,42 @@
+import { useState } from "react";
 import logoImg from "/assets/logo.svg";
 export function App() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [errors, setErrors] = useState({ email: "", password: "" });
+
+  function handleSubmit(event) {
+    event.preventDefault();
+
+    let formIsValid = true;
+    let newErrors = { email: "", password: "" };
+
+    if (!email) {
+      newErrors.email = "O e-mail é obrigatório!";
+      formIsValid = false;
+    }
+
+    if (!/\S+@\S+\.\S+/.test(email)) {
+      newErrors.email = "Digite um email válido!";
+      formIsValid = false;
+    }
+
+    if (!password) {
+      newErrors.password = "A senha é obrigatória!";
+      formIsValid = false;
+    }
+
+    if (password.length > 6) {
+      newErrors.password = "A senha deve conter ao menos 6 caracteres!";
+      formIsValid = false;
+    }
+
+    if (!formIsValid) {
+      setErrors(newErrors);
+      return;
+    }
+  }
+
   return (
     <>
       <section>
@@ -11,19 +48,40 @@ export function App() {
               Faça login ou registre-se para começar a construir seus projetos
               ainda hoje
             </p>
-            <form>
+
+            <form obSubmit={handleSubmit}>
               <label htmlFor="email">E-mail</label>
-              <input type="email" placeholder="Digite seu e-mail" />
+              <input
+                type="email"
+                placeholder="Digite seu e-mail"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                className={errors.email ? "error" : ""}
+              />
+              {errors.email && <span className="error">{errors.email}</span>}
 
               <div className="iForgor">
                 <label htmlFor="password">Senha</label>
                 <a href="">Esqueceu a senha?</a>
               </div>
-              <input type="password" placeholder="Digite sua senha"/>
+              <input
+                type="password"
+                placeholder="Digite sua senha"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                className={errors.password ? "error" : ""}
+              />
+              {errors.password && (
+                <span className="error">{errors.password}</span>
+              )}
 
-              <button type="submit" className="signIn">Entrar</button>
+              <button type="submit" className="signIn">
+                Entrar
+              </button>
             </form>
-            <p className="signUp">Ainda não tem uma conta? <a href="">Inscreva-se</a></p>
+            <p className="signUp">
+              Ainda não tem uma conta? <a href="">Inscreva-se</a>
+            </p>
           </div>
         </div>
         <div className="s-img"></div>
